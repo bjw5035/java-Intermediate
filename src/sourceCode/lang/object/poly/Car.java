f065191f3108716e28c0066b7f923837d8557fe7
Jae Woo <bjw5035@gmail.com>
2025-01-13 14:53:31 +0900

    feat : 김영한 자바 중급(Object 클래스)

---
+++ src/sourceCode/lang/object/poly/Car.java
@@ -0,0 +1,8 @@
+package src.sourceCode.lang.object.poly;
+
+public class Car {
+
+    public void sound() {
+        System.out.println("¸Û¸Û");
+    }
+}
---
+++ src/sourceCode/lang/object/poly/Dog.java
@@ -0,0 +1,9 @@
+package src.sourceCode.lang.object.poly;
+
+public class Dog {
+
+    public void sound() {
+        System.out.println("자동차 이동");
+    }
+
+}
