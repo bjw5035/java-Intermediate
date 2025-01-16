59f8bc45f28c6a3f9c84ff059f445d5917a87100
Jae Woo <bjw5035@gmail.com>
2025-01-16 19:38:46 +0900

    feat : 김영한 자바 중급(String 클래스)

---
+++ src/sourceCode/lang/string/builder/StringBuilderMain1_2.java
@@ -0,0 +1,14 @@
+package src.sourceCode.lang.string.builder;
+
+public class StringBuilderMain1_2 {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        String st = sb.append("A").append("B").append("C").append("D").toString();
+        System.out.println("st = " + st);
+
+        sb.insert(4, "java");
+        System.out.println("insert = " + sb);
+    }
+
+}
