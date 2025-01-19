fc5e0142ab1b3aabfe6a4dd67ee603d58ed97932
Jae Woo <bjw5035@gmail.com>
2025-01-19 16:30:56 +0900

    feat : 김영한 자바 중급(열거형 ENUM 리팩토링3)

---
+++ src/sourceCode/enumeration/ref3/EnumRefMain3_1.java
@@ -0,0 +1,18 @@
+package src.sourceCode.enumeration.ref3;
+
+import src.sourceCode.enumeration.ref2.DiscountService;
+import src.sourceCode.enumeration.ref2.Grade;
+
+public class EnumRefMain3_1 {
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(src.sourceCode.enumeration.ref2.Grade.BASIC, price);
+        int gold = discountService.discount(src.sourceCode.enumeration.ref2.Grade.GOLD, price);
+        int diamond = discountService.discount(Grade.DIAMOND, price);
+        System.out.println("BASIC 등급의 할인 금액: " + basic);
+        System.out.println("GOLD 등급의 할인 금액: " + gold);
+        System.out.println("DIAMOND 등급의 할인 금액: " + diamond);
+    }
+
+}
