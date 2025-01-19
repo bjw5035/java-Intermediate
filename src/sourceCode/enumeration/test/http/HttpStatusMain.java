69e31f06971aac1c3db920924e6527115fe3efbb
Jae Woo <bjw5035@gmail.com>
2025-01-19 17:08:13 +0900

    feat : 김영한 자바 중급(열거형 ENUM - 문제풀이2)

---
+++ src/sourceCode/enumeration/test/http/HttpStatusMain.java
@@ -0,0 +1,20 @@
+package src.sourceCode.enumeration.test.http;
+
+import java.util.Scanner;
+
+public class HttpStatusMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter HTTP status code: ");
+        int status = scanner.nextInt();
+
+        HttpStatus httpStatus = HttpStatus.findByCode(status);
+        if (httpStatus == null) {
+            System.out.println("정의되지 않은 코드");
+        } else {
+            System.out.println(httpStatus.getCode() + " " + httpStatus.getMessage());
+            System.out.println("isSuccess = " + httpStatus.isSuccess());
+        }
+    }
+
+}
