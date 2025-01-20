48dcc5e7a634db864f52552b6955e993fd1387c2
Jae Woo <bjw5035@gmail.com>
2025-01-20 16:16:19 +0900

    feat : 김영한 자바 중급(정적 중첩 클래스 - 지역클래스 예제)

---
+++ src/sourceCode/session9/nested/local/LocalOuterV1.java
@@ -0,0 +1,33 @@
+package src.sourceCode.session9.nested.local;
+
+public class LocalOuterV1 {
+
+    private int outInstanceVar = 3;
+
+    public void process(int paramVar) {
+        // 지역변수
+        int localVar = 1;
+
+        class LocalPrinter {
+            // 멤버변수
+            int value = 0;
+
+            public void printData() {
+                System.out.println(value);
+                System.out.println(localVar);
+                System.out.println(paramVar);
+                System.out.println(outInstanceVar);
+            }
+
+        }
+
+        LocalPrinter printer = new LocalPrinter();
+        printer.printData();
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV1 localOuterV1 = new LocalOuterV1();
+        localOuterV1.process(2);
+    }
+
+}
---
+++ src/sourceCode/session9/nested/local/Printer.java
@@ -0,0 +1,7 @@
+package src.sourceCode.session9.nested.local;
+
+public interface Printer {
+
+    public void print();
+
+}
