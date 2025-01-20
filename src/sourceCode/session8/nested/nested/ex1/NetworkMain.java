2369bc793d8632c9cee799fde0e3aa7f71c1f1a7
Jae Woo <bjw5035@gmail.com>
2025-01-20 13:51:52 +0900

    feat : 김영한 자바 중급(정적 중첩 클래스 예제)

---
+++ src/sourceCode/session8/nested/nested/ex1/NetworkMain.java
@@ -0,0 +1,10 @@
+package src.sourceCode.session8.nested.nested.ex1;
+
+public class NetworkMain {
+
+    public static void main(String[] args) {
+        Network network = new Network();
+        network.sendMessage("hello");
+    }
+
+}
