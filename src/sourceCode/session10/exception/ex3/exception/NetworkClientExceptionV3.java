62f1b4d4a306e0f869901e860f845dc238a833b8
Jae Woo <bjw5035@gmail.com>
2025-01-22 17:32:43 +0900

    feat : 김영한 자바 중급(예외처리2 - 시작)

---
+++ src/sourceCode/session10/exception/ex3/exception/NetworkClientExceptionV3.java
@@ -0,0 +1,9 @@
+package src.sourceCode.session10.exception.ex3.exception;
+
+public class NetworkClientExceptionV3 extends Exception {
+
+    public NetworkClientExceptionV3(String message) {
+        super(message);
+    }
+
+}
